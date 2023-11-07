ececba12776877e163debeac89ad51a419870155
Marco <55298054+marco-giglio@users.noreply.github.com>
2023-11-07 21:49:44 +0000

    Add book pages and read status, and implement toggle and remove book functions

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const myLibrary = [
-    {
-        title: 'The Old Man and the Sea', 
-        author: 'Earnest Hemingway' }, 
-    {
-        title: "Hitchhiker's Guide to the Galaxy",
-        author: "Douglas Adams"
-    }
-]
+    new Book('The Old Man and the Sea', 'Earnest Hemingway', 200, false), 
+    new Book("Hitchhiker's Guide to the Galaxy", "Douglas Adams", 354, true)
+];
 
-function Book(title, author) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
+    this.pages = pages; 
+    this.read = read;
 
+}
+
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead();
+    displayBooks();
 }
 
 function addBookToLibrary() {
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
 
-    let newBook = new Book(title, author)
+    let newBook = new Book(title, author, pages, read)
 
     myLibrary.push(newBook)
 
@@ -54,9 +62,18 @@ function displayBooks() {
             <h5>${book.author}</h5>
          </div>
          <div class="card-body">
-            <p> 200 pages </p>
+            <p> ${book.pages} pages </p>
+            <p> ${book.read ? "yes" : "no"} </p>
          </div>
+         <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+         <button class="toggle-btn" onclick="toggleRead(${i})">Toggle Read</button>
         `; 
         library.appendChild(bookElement); 
     }
+}
+
+
+function removeBook(index) {
+    myLibrary.splice(index, 1); 
+    displayBooks();
 }
