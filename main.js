051d2e50f55c6afd81f4ca752a8b6bb442d33dbc
Marco <55298054+marco-giglio@users.noreply.github.com>
2023-11-07 20:16:58 +0000

    Add initial HTML, CSS, and JavaScript files for Library project

---
+++ main.js
@@ -0,0 +1,17 @@
+const myLibrary = [
+    {
+        title: 'The Old Man and the Sea', 
+        author: 'Earnest Hemingway' }, 
+    {
+        title: "Hitchhiker's Guide to the Galaxy",
+        author: "Douglas Adams"
+    }
+]
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
